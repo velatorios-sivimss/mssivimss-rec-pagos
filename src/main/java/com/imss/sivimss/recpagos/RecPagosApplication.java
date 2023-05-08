8161cdc9a1dd41ecaa8fef8ef9ec1a56fc012eed
Irwin <irwin.ceballos@people-media.com.mx>
2023-05-08 14:06:20 -0600

    Correcciones de sonar

--- src/main/java/com/imss/sivimss/recpagos/RecPagosApplication.java
+++ src/main/java/com/imss/sivimss/recpagos/RecPagosApplication.java
@@ -14,7 +14,7 @@ import com.imss.sivimss.recpagos.util.NoRedirectSimpleClientHttpRequestFactory;
 @SpringBootApplication
 public class RecPagosApplication {
 
-	public static void main() {
+	public static void main(String[] args) {
 		SpringApplication.run(RecPagosApplication.class);
 	}
 
